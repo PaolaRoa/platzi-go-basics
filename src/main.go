874e06aeffaace62b313f289ddd38d57b2c14c7d
PaolaRoa <yproab@gmail.com>
2023-01-17 11:20:18 -0500

    consts and vars

--- src/main.go
+++ src/main.go
@@ -5,6 +5,32 @@ import "fmt"
 func main() {
 	//go build src/main.go
 	//go run src/main.go
+	//Declaracion constante
+	const pi float64 = 3.14
 
-	fmt.Println("Hola mundo")
+	const pi2 = 3.14
+
+	fmt.Println(pi, pi2)
+
+	//declaracion variables
+	//enteras
+	//no declarada anteriormente := crea y asigna
+	base := 12
+	var altura int = 14
+	//Zero values -> asigna valor por defecto
+	var area int
+	var a float64
+	var b string
+	var c bool
+
+	fmt.Println(base, altura, area)
+
+	fmt.Println(area, a, b, c)
+
+	//area
+	const basecuadrado = 10
+	areaCuadrado := basecuadrado * basecuadrado
+
+	fmt.Println(areaCuadrado)
+
 }
